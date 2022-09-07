5dc1475f212ebf7682b6c2da0988a9dae66373e8
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-09-07 11:38:31 +0300

    add buble sort function

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -8,27 +8,45 @@ export default function Visualizer() {
   const [values, setValues] = useState(initialArray);
 
   return (
-    <div className="container">
-      {values.map((x, i) => (
-        <div>
-          <div
-            key={`${x} - ${i}`}
-            style={{ height: `${x}px` }}
-            className="item"
-          ></div>
-          {x}
-        </div>
-      ))}
-      <button onClick={() => setValues(generateArray())}>
-        Generate New Array
-      </button>
+    <div>
+      <div className="header">
+        <button onClick={() => setValues(generateArray())} className="btn">
+          Generate New Array
+        </button>
+        <button className="btn" onClick={() => bubbleSort(values)}>
+          Bubble Sort
+        </button>
+      </div>
+      <div className="container">
+        {values.map((x, i) => (
+          <div key={`${i}`} style={{ height: `${x}px` }} className="item">
+            {x}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
+const bubbleSort = (arr) => {
+  const domArray = document.getElementsByClassName("item");
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  console.log(arr);
+};
+
 function generateArray() {
   const arr = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 5; i++) {
     arr.push(getRandomNumber());
   }
 
