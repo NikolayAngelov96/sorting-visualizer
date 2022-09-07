73cf0f990f1571c97a4d6a992785b7ab0923f361
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-09-07 16:01:10 +0300

    implement bubble sort

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -10,12 +10,16 @@ export default function Visualizer() {
   return (
     <div>
       <div className="header">
-        <button onClick={() => setValues(generateArray())} className="btn">
+        <button
+          onClick={() => setValues(generateArray())}
+          className="btn generate-btn"
+        >
           Generate New Array
         </button>
         <button className="btn" onClick={() => bubbleSort(values)}>
           Bubble Sort
         </button>
+        <button className="btn">Selection Sort</button>
       </div>
       <div className="container">
         {values.map((x, i) => (
@@ -28,25 +32,45 @@ export default function Visualizer() {
   );
 }
 
-const bubbleSort = (arr) => {
-  const domArray = document.getElementsByClassName("item");
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const bubbleSort = async (arr) => {
+  const domArray = [...document.getElementsByClassName("item")];
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1 - i; j++) {
+      domArray[j].style.backgroundColor = "red";
+      domArray[j + 1].style.backgroundColor = "red";
+      await sleep(500);
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+
+        domArray[j].style.height = arr[j] + "px";
+        domArray[j].style.backgroundColor = "red";
+        domArray[j].textContent = arr[j];
+
+        domArray[j + 1].style.height = arr[j + 1] + "px";
+        domArray[j + 1].style.backgroundColor = "red";
+        domArray[j + 1].textContent = arr[j + 1];
+        await sleep(500);
       }
+
+      domArray[j].style.backgroundColor = "blue";
+      domArray[j + 1].style.backgroundColor = "blue";
     }
+    await sleep(500);
   }
 
-  console.log(arr);
+  domArray.forEach((e) => (e.style.backgroundColor = "blue"));
 };
 
 function generateArray() {
   const arr = [];
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i++) {
     arr.push(getRandomNumber());
   }
 
